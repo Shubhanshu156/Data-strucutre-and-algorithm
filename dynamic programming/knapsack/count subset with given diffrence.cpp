828280ec7010f5a6512bf0320b2c770fd1c3d561
Shubhanshu sharma <shubhanshusharmra2712@gmail.com>
2021-06-26 20:43:05 +0530

    add

---
+++ dynamic programming/knapsack/count subset with given diffrence.cpp
@@ -0,0 +1,60 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    
+int t[20][20];
+int topdown(int array[],int sum,int n ){
+    int a[n+1][sum+1];
+    for (int i = 0; i < n+1; i++)
+    {
+        for (int j = 0; j < sum+1; j++)
+        {
+            if (i==0){
+                a[i][j]=0;
+            }
+            if (j==0){
+                a[i][j]=1;
+            }
+        }
+    }
+    for (int i = 1; i < n+1; i++)
+    {
+        /* code */
+        for (int j = 1; j < sum+1; j++)
+        {
+            /* code */
+            if (array[i-1]<=j){
+                a[i][j]=a[i-1][j-array[i-1]]+a[i-1][j];
+            }
+            else{
+                a[i][j]=a[i-1][j];
+            }
+        }
+        
+    }
+    return a[n][sum];
+    
+    
+
+}
+
+    int main(){
+        
+        
+        int weight[3]={1,2,3};
+        int sum=0;
+        for (int i = 0; i < sizeof(weight)/sizeof(weight[0]); i++)
+        {
+            /* code */
+            sum+=weight[i];
+        }
+        
+        
+        int capacity=3;
+        int diff=1;
+
+        int n=3;
+        int newsum=(sum+diff)/2;//S1-S2=diff
+                                //S1+S2=sum>>S1=(sum+diff)/2 count s1
+       cout<<topdown(weight,newsum,n);
+    
+    }
